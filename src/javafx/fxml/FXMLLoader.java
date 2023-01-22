c319099efdf66879c5333efe2ef98a4e9fb25563
Codi-Haraguchi <codi798@gmail.com>
2023-01-22 17:46:39 -0300

    Main view

---
+++ src/javafx/fxml/FXMLLoader.java
@@ -0,0 +1,18 @@
+package javafx.fxml;
+
+import java.net.URL;
+
+import javafx.scene.Parent;
+
+public class FXMLLoader {
+
+	public FXMLLoader(URL resource) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Parent load() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
